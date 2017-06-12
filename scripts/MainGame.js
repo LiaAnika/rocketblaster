9dd13894bbbd374b2c4106f4e725529a1ddef62d
LiaAnika <anika.chavda@yahoo.com>
2017-06-12 18:24:01 +0100

    finished mainmenu

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -9,8 +9,27 @@ var bullets;
 var fireRate = 100;
 var nextFire = 0;
 
+//score & Lives
+var score;
+var lifeTotal;
+var scoreText;
+var lifeTotalText;
+
+//audio
+var music;
+var bulletAudio;
+var explosionAudio;
+
+//timer 
+var seconds;
+var timer;
+var timerText;
+
 // misc variables
 var cursors; // =keyboard
+var gameOverText;
+var restartButton;
+var gameOver;
 
 BasicGame.Game.prototype = {
 
@@ -53,31 +72,90 @@ BasicGame.Game.prototype = {
 			bullets.setAll('anchor.y', 0.5);
 			bullets.setAll('outOfBoundsKill', true);
 			bullets.setAll('checkWorldBounds', true);
+		
+			scoreText = this.add.text(16, 16, 'Score: 0', {
+				font: '32px arial', 
+				fill: '#fff'
+			});
+			
+			score = 0;
+			scoreText.text = "Score: " + score; 
+		
+			lifeTotalText = this.add.text(this.world.width - 150, 16, 'Live: 3', {
+				font: '32px arial',
+				fill: '#ff'
+			});
+		
+			lifeTotal = 3;
+			lifeTotalText.text = 'Lives:' + lifeTotal;
+			timerText = this.add.text(350, 16, 'Time: 0', {
+				font: '32px arial', 
+				fill: '#fff'
+	});
+		
+			//setup timer
+			timer = this.time.create(false);	
+			seconds = 0;
+			timerText.text = 'Time: ' + seconds;
+			gameOverText = this.add.text(this.world.centerX, this.world.center Y-50, 'Game Over', {
+				font: '96px arial',
+				fill: '#fff',
+				align: 'center'
+});
+			
+			gameOverText.anchor.set(0.5);
 
+			//hides the gameState text
+			gameOverText.visible = false;
+			gameOver = false;
 
-			// setting the keyboard to accpect LEFT RIGHT SPACE input
-			this.input.keyboard.addKeyCapture([Phaser.keyboard.LEFT, Phaser.keyboard.RIGHT, Phaser.keyboard.SPACEBAR]);
+			//Create a restart button and hide on screen
+			restartButton = this.add.button((this.world.width / 2),
+			(this.world.height / 2)+50, 'startButton', this.restartGame);
+			restartButton.anchor.set(0.5);
+			restartButton.visible = false;
+
+			//Setting the keyboard to accept LEFT, RIGHT and SPACE input
+			this.input.keyboard.addKeyCapture([Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR]);
 			cursors = this.input.keyboard.createCursorKeys();
-	},
+
+			//Load the audio into memory, starting music
+			bulletAudio = this.add.audio('bullet');
+			explosionAudio = this.add.audio('explosion');
+			music = this.add.audio('music', 1, true);
+			music.play('', 0, 1, true);
+
+			//Set a TimerEvent to occur every second and start the timer
+			timer.loop(1000, this.updateTimer, this);
+			timer.start();
+},
 
 	update: function () {
+		// background
+		this.starfield.tilePosition.y += 2;
+		//if lifeTotal is less than 1 
+		if (lifeTotal < 1 || seconds == 60 || gameOver===true) {
+			this.gameOver();
+		}
+		
 		//execute 'createUfo','createLife','moveShip','collisionDetection' function
+		else {
 		this.createUfo();
 		this.createLife();
 		this.moveShip();
 		this.collisionDetection();
-
+		}
 	},
 
 		moveShip: function (){
 			if (cursors.left.isDown)
-						ship.body.velocity.x = -200;
+			ship.body.velocity.x = -200;
 		}
 	 		else if (cursors.right.isDown){
-				ship.body.velocity.x = 200;
+			ship.body.velocity.x = 200;
 			}
 			else {
-				ship.body.velocity.x = 0;
+			ship.body.velocity.x = 0;
 			}
 
 			if (this.input.keyboard.isDown(Phaser.keyboard.SPACEBAR)) {this.fireBullet();
@@ -98,6 +176,7 @@ BasicGame.Game.prototype = {
 					//random velocity
 					ufo.body.velocity.y = this.rnd.integerInRange(200, 300);
 				}
+			},
 	},
 				createLife: function() {
 					//random numbers between 0-500
@@ -118,6 +197,7 @@ BasicGame.Game.prototype = {
 						var bullet = bullets.getFirstExists(false);
 						bullet.reset(ship.x, ship.y);
 						bullet.body.velocity.y = -400;
+						bulletAudio.play();
 					}
 				},
 				//check for collisions
@@ -129,12 +209,55 @@ BasicGame.Game.prototype = {
 
 				//collisions between player and ufos
 				collideUfo: function (ship, ufo){
+					explosionAudio.play();
 					ufo.kill();
-					bullet.kill();
+					var animation = this.add.sprite(ufo.body.x, ufo.body.y, 'kaboom');
+					animation.animations.add('explode');
+					animation.animations.play('explode', 30, false, true);
+					lifeTotal--;
+					lifeTotalText.text = 'Lives: ' + lifeTotal;
+					//
+					gameOver=True;
 				},
-
-				//collision between player and life
+					destroyUfo: function (bullet, ufo) {
+						explosionAudio.play();
+						ufo.kill();
+						bullet.kill();
+						var animation = this.add.sprite(ufo.body.x, ufo.body.y, 'kaboom');
+						animation.animations.add('explode');
+						animation.animations.play('explode', 30, false, true);
+						score += 100;
+						scoreText.text = 'Score: ' + score;
+					}, 
+					
+						//collision between player and life
 				collectLife: function (ship, life){
 					life.kill();
-				}
-};
+					lifeTotal++;
+					lifeTotalText.text = 'Lives: ' + lifeTotal;
+					var animation = this.add.sprite(life.body.x, life.body.y, 'lifeAnimation');
+					animation.animations.add('lifeAnimation');
+					animation.animations.play('lifeAnimation', 30, false, true);
+				},
+					
+				  udateTimer: function () {
+					  seconds++;
+					  timerText.text= 'Time: ' + seconds;
+				  },
+					  
+					  gameOver: function () {
+						  ship.body.velocity.x = 0;
+						  ship.body.x = (this.world.width/2)-(ship.body.width/2);
+						  ufos.callAll('kill');
+						  lives.callAll('kill');
+						  bullets.callAll('kill');
+						  music.stop();
+						  gameOverText.visible = true;
+						  restartButton.visible = true;
+						  timer.stop();
+		
+				},
+					restartGame: function () {
+						this.game.state.start('Game');
+					}
+			};
