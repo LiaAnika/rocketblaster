b04d2183f42c4aea2eec5aa2cad428b9eec0fdc3
LiaAnika <anika.chavda@yahoo.com>
2017-06-16 11:45:01 +0100

    Update MainMenu.js

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -7,7 +7,7 @@ var logo;
 BasicGame.MainMenu.prototype = {
     
     create: function () {
-        starfield = this. add.tileSprite(0, 0, 800, 600, 'starfield'); 
+        starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield'); 
         logo = this.add.sprite((this.world.width / 2), (this.world.height / 2) -150, 'logo');
         logo.anchor.setTo(0.5,0.5);
         startButton = this.add.button((this.world.width / 2),(this.world.height /2) + 50, 'startButton', this.startGame);
