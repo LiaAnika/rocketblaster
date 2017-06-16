04d152993fe7809548c128bbb272c220bcfc8cc0
LiaAnika <anika.chavda@yahoo.com>
2017-06-16 11:13:58 +0100

    Update MainGame.js

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -44,7 +44,7 @@ BasicGame.Game.prototype = {
         this.starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
         //adding the ship onto the screen, set the physics and the boundarys 
         this.ship = this.add.sprite((this.world.width / 2), this.world.height - 50, 'ship');
-        ship.achor.setTo(0.5,0);
+        ship.anchor.setTo(0.5,0);
         this.physics.enable(ship, Phaser.Physics.ARCADE);
         ship.body.collideWorldBounds = true;
         
