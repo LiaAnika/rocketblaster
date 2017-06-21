a6c66fe0ac087c88d179e260f182603ff64b6ac1
LiaAnika <anika.chavda@yahoo.com>
2017-06-21 11:34:19 +0100

    update

--- scripts/MainGame.js
+++ scripts/MainGame.js
@@ -271,12 +271,12 @@ BasicGame.Game.prototype = {
                             restartGame: function () {
                                 this.game.state.start('Game');
                             },
-                    //render: function() {
+                   render: function() {
                         //sprite debug info
-                       // this.game.debug.bodyInfo(ship, 32, 100);
-                        //this.game.debug.spriteBounds(ship);
-                    //}
-}; 
+                       this.game.debug.bodyInfo(ship, 32, 100);
+                        this.game.debug.spriteBounds(ship);
+                    }
+                   }; 
 
 
 
